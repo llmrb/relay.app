f3879c1f168b043a5804b795cd996ef326fa1833
Robert <0x1eef@hardenedbsd.org>
2026-03-27 01:17:38 -0300

    Test Relay loader reloading with app constant

--- test/zeitwerk_reloading_test.rb
+++ test/zeitwerk_reloading_test.rb
@@ -1,41 +1,49 @@
 # frozen_string_literal: true
 
 require "tmpdir"
+require "fileutils"
 require "test/unit"
 require "zeitwerk"
 
 class ZeitwerkReloadingTest < Test::Unit::TestCase
-  def test_loader_can_reload_updated_constants_when_reloading_is_enabled
+  def test_loader_reloads_updated_relay_constants
     Dir.mktmpdir do |dir|
-      File.write(File.join(dir, "example.rb"), <<~RUBY)
-        class Example
-          def self.value
-            :before
+      app_dir = File.join(dir, "app")
+      routes_dir = File.join(app_dir, "routes")
+      FileUtils.mkdir_p(routes_dir)
+
+      File.write(File.join(routes_dir, "example.rb"), <<~RUBY)
+        module Relay::Routes
+          class Example
+            def self.value
+              :before
+            end
           end
         end
       RUBY
 
       loader = Zeitwerk::Loader.new
-      loader.push_dir(dir)
+      loader.push_dir(app_dir, namespace: Relay)
       loader.enable_reloading
       loader.setup
 
-      assert_equal :before, Example.value
+      assert_equal :before, Relay::Routes::Example.value
 
-      File.write(File.join(dir, "example.rb"), <<~RUBY)
-        class Example
-          def self.value
-            :after
+      File.write(File.join(routes_dir, "example.rb"), <<~RUBY)
+        module Relay::Routes
+          class Example
+            def self.value
+              :after
+            end
           end
         end
       RUBY
 
       loader.reload
 
-      assert_equal :after, Example.value
+      assert_equal :after, Relay::Routes::Example.value
     ensure
       loader&.unload
-      Object.send(:remove_const, :Example) if Object.const_defined?(:Example)
     end
   end
 end
